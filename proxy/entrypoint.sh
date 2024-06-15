bada10327dc1c71ac8d8757f1370fcabc745c025
AmarKanth <amarkanth.bollu@gmail.com>
2024-06-15 15:24:39 +0530

    changes in file stracture

---
+++ proxy/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+echo "Checking for fullchain.pem"
+if [ ! -f "/etc/letsencrypt/live/practicepurpose.xyz/fullchain.pem" ]; then
+    echo "No SSL cert, enabling HTTP only..."
+    cp ./http.conf /etc/nginx/nginx.conf
+else
+    echo "SSL cert exists, enabling HTTPS..."
+    cp ./https.conf /etc/nginx/nginx.conf
+fi
+
+nginx-debug -g 'daemon off;'
